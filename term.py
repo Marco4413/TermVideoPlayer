344f74768ede2ac8f40612d72bbe30c0b26524fd
Marco <46133335+Marco4413@users.noreply.github.com>
2024-01-14 13:55:04 +0100

    Added term.next_line

--- term.py
+++ term.py
@@ -10,5 +10,8 @@ def reset_background_color(out: TextIO = stdout):
 def set_cursor(x: int, y: int, out: TextIO = stdout):
     out.write(f"\x1B[{y};{x}H")
 
+def next_line(n: int = 1, out: TextIO = stdout):
+    out.write(f"\x1B[{n}E")
+
 def clear_all(out: TextIO = stdout):
     out.write("\x1B[2J")
